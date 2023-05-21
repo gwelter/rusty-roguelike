2cd930e0f0e68d1a4862683b18092a0faa06913d
Guilherme Welter <guilherme.a.welter@gmail.com>
2023-05-21 17:03:48 -0300

    Adding the player and moving it around the map

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 mod map;
+mod map_builder;
+mod player;
+
 use bracket_lib::prelude::*;
 
 mod prelude {
@@ -6,28 +9,36 @@ mod prelude {
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
 }
 use prelude::*;
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-        .with_title("Hello Rust World")
+        .with_title("Rusty Roguelike")
         .with_fps_cap(30.0)
         .build()?;
     main_loop(context, State::new())
---
+++ src/map_builder.rs
@@ -0,0 +1,8 @@
+use crate::prelude::*;
+const NUM_ROOMS: usize = 20;
+
+pub struct MapBuilder {
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub player_start: Point,
+}
