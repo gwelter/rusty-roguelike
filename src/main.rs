32a3ade061fbee4d1f001bddbf177e224ad1d44c
Guilherme Welter <guilherme.a.welter@gmail.com>
2023-05-27 14:59:23 -0300

    Spawning monster at the center of every room besides the player room

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,12 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|room| room.center())
+            .for_each(|position| spawn_monster(&mut ecs, &mut rng, position));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
 
