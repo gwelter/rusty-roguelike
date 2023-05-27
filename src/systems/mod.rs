109ebb88d7544cad605bf96223a0f9349ad3aaea
Guilherme Welter <guilherme.a.welter@gmail.com>
2023-05-27 19:18:37 -0300

    Colliding with monsters and removing them

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
@@ -8,5 +9,6 @@ pub fn build_scheduler() -> Schedule {
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(collisions::collisions_system())
         .build()
 }
