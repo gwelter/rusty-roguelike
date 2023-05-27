985c2ffdbe5c19c5a811bba16fb1c2a165f627b7
Guilherme Welter <guilherme.a.welter@gmail.com>
2023-05-27 10:05:54 -0300

    Added the entities renderer and using it

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+mod entity_render;
 mod map_render;
 mod player_input;
 
@@ -6,5 +7,6 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
         .build()
 }
