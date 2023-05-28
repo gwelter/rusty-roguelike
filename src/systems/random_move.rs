2166ace1a77d4e5590f32fead562118653c19c50
Guilherme Welter <guilherme.a.welter@gmail.com>
2023-05-28 18:56:31 -0300

    Refactoring state managemente to use messages insetead of direct write access

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -1,11 +1,11 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
-    let mut movers = <(&mut Point, &MovingRandomly)>::query();
-    movers.iter_mut(ecs).for_each(|(position, _)| {
+pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer, #[resource] map: &Map) {
+    let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
+    movers.iter_mut(ecs).for_each(|(entity, position, _)| {
         let mut rng = RandomNumberGenerator::new();
         let destination = match rng.range(0, 4) {
             0 => Point::new(-1, 0),
@@ -13,8 +13,12 @@ pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
             2 => Point::new(0, -1),
             _ => Point::new(0, 1),
         } + *position;
-        if map.can_enter_tile(destination) {
-            *position = destination;
-        }
+        commands.push((
+            (),
+            WantsToMove {
+                entity: *entity,
+                destination,
+            },
+        ));
     })
 }
